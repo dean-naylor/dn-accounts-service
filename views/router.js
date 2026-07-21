4fc3e8c523b78f8acc3cfd1872664a463d2fcd03
dean naylor <ME\du_348530-1781186061@a-hi4unlasmgom.me.ga.co>
2026-07-21 17:27:31 +0100

    built app up to end of first session, router has routes for accounts etc

---
+++ views/router.js
@@ -0,0 +1,14 @@
+import express from "express";
+import accountsController from "../controllers/accountsController.js";
+
+const Router = express.Router();
+
+Router.route("/accounts")
+    .get(accountsController.getAllAccounts)
+    .post(accountsController.createAccount);
+Router.route("/accounts/:id")
+    .get(accountsController.getAccountById)
+    .put(accountsController.updateAccountById)
+    .delete(accountsController.deleteAccountById);
+
+export default Router;
